80d635b9da62e267e71a3f3b29fc98facd9f82c3
Deivid Rafael <deivid.rosario@hotmail.com>
2024-10-01 00:01:06 -0300

    Review variable env

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 // Configuração do pool do PostgreSQL
 const pool = new Pool({
-  user: process.env.,
+  user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASS,
